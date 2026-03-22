04c763d62149d2d0ed65a78b3be5b777d6dd2e81
Ayush-31r <rai4ayush@gmail.com>
2026-03-23 03:02:33 +0530

    Include 20F/6K filings

--- rag/build_index.py
+++ rag/build_index.py
@@ -22,9 +22,8 @@ def get_cik(ticker):
 
 def get_filing_url(cik):
     padded_cik = str(cik).zfill(10)
-    
     url = f"https://data.sec.gov/submissions/CIK{padded_cik}.json"
-    response = requests.get(url,headers=HEADERS)
+    response = requests.get(url, headers=HEADERS)
     data = response.json()
 
     filings = data["filings"]["recent"]
@@ -32,19 +31,18 @@ def get_filing_url(cik):
     ten_q_url = None
 
     for form, accession, primary_doc in zip(filings["form"], filings["accessionNumber"], filings["primaryDocument"]):
-        if form == "10-K" and ten_k_url is None:
-            accession_clean = accession.replace("-","")
+        if form in ("10-K", "20-F") and ten_k_url is None:
+            accession_clean = accession.replace("-", "")
             ten_k_url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_clean}/{primary_doc}"
 
-        if form == "10-Q" and ten_q_url is None:
-            accession_clean = accession.replace("-","")
+        if form in ("10-Q", "6-K") and ten_q_url is None:
+            accession_clean = accession.replace("-", "")
             ten_q_url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_clean}/{primary_doc}"
 
         if ten_k_url and ten_q_url:
             break
 
-    return ten_k_url,ten_q_url
-
+    return ten_k_url, ten_q_url
 
 def fetch_and_clean(url):
     
@@ -60,7 +58,13 @@ def fetch_and_clean(url):
 def build_save_index(ticker):
     cik = get_cik(ticker)
     ten_k_url,ten_q_url = get_filing_url(cik)
-    text = fetch_and_clean(ten_k_url) + fetch_and_clean(ten_q_url)
+    if not ten_k_url and not ten_q_url:
+        return "No SEC filings found for the ticker"
+    text = ""
+    if ten_k_url:
+        text += fetch_and_clean(ten_k_url)
+    if ten_q_url:
+        text += fetch_and_clean(ten_q_url)
     spliter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap = 200)
     chunks = spliter.split_text(text)
 
